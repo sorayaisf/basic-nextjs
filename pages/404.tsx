ec0d84880fa810738ff5c8283af2021d4a46f1e6
Soraya Isfandiari <soraya.isf@gmail.com>
2022-06-28 15:52:23 +0700

    learning nextjs

---
+++ pages/404.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router';
+
+export default function Custom404() {
+    const router = useRouter();
+    useEffect(
+        () => {
+            setTimeout(() => {
+                router.push('/')
+            }, 2000)
+        }, []
+    )
+    return (
+        <div>
+            <h1 className='title-not-found'>Ooops...</h1>
+            <h1 className='title-not-found'>Page not found.</h1>
+
+        </div>
+    )
+}
